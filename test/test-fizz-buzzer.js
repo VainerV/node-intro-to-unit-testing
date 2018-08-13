d4a2ee5b54401a4555f8ab57aeb4cce156b60002
Vadim Vainer <vainerxv@gmail.com>
2018-08-13 16:33:38 -0700

    Updated version of fizzBuzz test

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -10,31 +10,54 @@ describe('fizzBuzzer', function (input) {
 
         // check for result match 
 
-        it('should return FizzBuzz if is divisible by 3 and 5', function () {
-            assert.equal('FizzBuzz', fizzBuzzer(15));
+
+
+        /* it('should return FizzBuzz if is divisible by 3 and 5', function () {
+             assert.equal('FizzBuzz', fizzBuzzer(15));
+         });
+         it('should return Fizz if is divisible by 3', function () {
+             assert.equal('Fizz', fizzBuzzer(3));
+         });
+         it('should return Buzz if is divisible by 5', function () {
+             assert.equal('Buzz', fizzBuzzer(5));
+         });
+ 
+         // check for non numeric input 
+         it('should raise error if args not numbers', function () {
+             // range of bad inputs where not both are numbers
+             assert.equal('string', fizzBuzz(0));
+         });
+ 
+         it('should raise error if args not numbers', function () {
+             // range of bad inputs where not both are numbers
+             assert.equal('undefined', fizzBuzz(0));
+         });
+ 
+ */
+
+        const normalCase = [
+            { word: "fizz-buzz", expected: 15 },
+            { word: "fizz", expected: 3 },
+            { word: "buzz", expected: 5 }
+        ]
+        normalCase.forEach(function (input) {
+            const answer = fizzBuzzer(input.expected);
+            expect(answer).to.equal(input.word)
         });
-        it('should return Fizz if is divisible by 3', function () {
-            assert.equal('Fizz', fizzBuzzer(3));
-        });
-        it('should return Buzz if is divisible by 5', function () {
-            assert.equal('Buzz', fizzBuzzer(5));
-        });
-
-
-
-        // check for non numeric input 
-        it('should raise error if args not numbers', function () {
-            // range of bad inputs where not both are numbers
-            assert.equal('string', fizzBuzz(0));
-        });
-
-        it('should raise error if args not numbers', function () {
-            // range of bad inputs where not both are numbers
-            assert.equal('undefined', fizzBuzz(0));
-        });
-
-
 
 
     });
+
+    it('should raise error if args not numbers', function() {
+        // range of bad inputs where not both are numbers
+        const badInputs = [ 'string', undefined];
+        // prove that an error is raised for bad inputs
+        badInputs.forEach(function(input) {
+          expect(function() {
+            fizzBuzzer(input);
+          }).to.throw(Error);
+
+        });
+
+}) 
 })
